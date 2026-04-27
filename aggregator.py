758cbd8b55f93c53a949665e71c989e152923af9
Abhay Raina <75186151+abraina5@users.noreply.github.com>
2026-04-27 13:35:25 -0400

    Added Margin of Error calculations

--- aggregator.py
+++ aggregator.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import requests
 from datetime import date
 
@@ -12,6 +13,24 @@ df["end_date"] = pd.to_datetime(df["end_date"], errors="coerce")
 mask = ((pd.Timestamp(date.today()) - df["end_date"]).dt.days < 45)
 new = df[mask].copy()
 
-print(new.head())
-print(new.shape)
-print(df.shape)
+#Trump Favorability aggregator
+
+favorabilityMask = ((new["poll_type"] == "favorability") & (new['subject'] == "Donald Trump"))
+favorabilityDF = new[favorabilityMask].copy()
+
+favorabilityDF["approve_pct"] = favorabilityDF["answers"].apply(
+    lambda x: next((item["pct"] for item in x if item["choice"] == "Favorable"), None)
+)
+
+favorabilityDF["MoE"] = 1.96 * (
+    (favorabilityDF["approve_pct"] / 100) *
+    (1 - favorabilityDF["approve_pct"] / 100) /
+    favorabilityDF["sample_size"]
+) ** 0.5 * 100
+
+favorabilityDF["MoE"] = favorabilityDF["MoE"].round(3)
+
+print(favorabilityDF[["approve_pct","MoE"]])
+print(favorabilityDF.shape)
+print(favorabilityDF.columns)
+print(favorabilityDF["answers"].head())
