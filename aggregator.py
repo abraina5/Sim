771d95c27388364bc2874ffcb12ce59122870dbc
Abhay Raina <75186151+abraina5@users.noreply.github.com>
2026-05-21 10:09:36 -0400

    Started basic sim system
    Sim will operate by taking small microcosms of polling average and pvi and accumulating them by small precinct reportings at a time

--- aggregator.py
+++ aggregator.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import requests
 from datetime import date
+import time
+import random
 
 response = requests.get("https://api.votehub.com/polls")
 data = response.json()
@@ -59,6 +61,12 @@ estimated_votes_approval = (0.39*(weighted_approval / 100) + 0.3)
 print(estimated_votes_approval)
 print(sigma)
 
+# Senate elections
+
+
+
+
+
 # Older election day approval vs vote received (All from Gallup)
 
 # Structure is approval : vote total
@@ -86,3 +94,24 @@ electionDay = {
     0.43 : 0.468, # 2020 Trump
     0.37 : 0.483 # 2024 Biden approval : Harris votes
 }
+
+
+# Sim model
+tick_speed = 1
+total_ticks = 0
+
+states = {"Alabama":{"pvi":15,"precincts_in":0},"Alaska":{"pvi":9,"precincts_in":0},"Arizona":{"pvi":3,"precincts_in":0},"Arkansas":{"pvi":16,"precincts_in":0},"California":{"pvi":-13,"precincts_in":0},"Colorado":{"pvi":-4,"precincts_in":0},"Connecticut":{"pvi":-6,"precincts_in":0},"Delaware":{"pvi":-7,"precincts_in":0},"Florida":{"pvi":3,"precincts_in":0},"Georgia":{"pvi":1,"precincts_in":0},"Hawaii":{"pvi":-16,"precincts_in":0},"Idaho":{"pvi":18,"precincts_in":0},"Illinois":{"pvi":-7,"precincts_in":0},"Indiana":{"pvi":11,"precincts_in":0},"Iowa":{"pvi":6,"precincts_in":0},"Kansas":{"pvi":11,"precincts_in":0},"Kentucky":{"pvi":16,"precincts_in":0},"Louisiana":{"pvi":12,"precincts_in":0},"Maine":{"pvi":-2,"precincts_in":0},"Maryland":{"pvi":-13,"precincts_in":0},"Massachusetts":{"pvi":-14,"precincts_in":0},"Michigan":{"pvi":-1,"precincts_in":0},"Minnesota":{"pvi":-1,"precincts_in":0},"Mississippi":{"pvi":10,"precincts_in":0},"Missouri":{"pvi":10,"precincts_in":0},"Montana":{"pvi":11,"precincts_in":0},"Nebraska":{"pvi":13,"precincts_in":0},"Nevada":{"pvi":-1,"precincts_in":0},"New Hampshire":{"pvi":-1,"precincts_in":0},"New Jersey":{"pvi":-6,"precincts_in":0},"New Mexico":{"pvi":-5,"precincts_in":0},"New York":{"pvi":-11,"precincts_in":0},"North Carolina":{"pvi":1,"precincts_in":0},"North Dakota":{"pvi":19,"precincts_in":0},"Ohio":{"pvi":6,"precincts_in":0},"Oklahoma":{"pvi":20,"precincts_in":0},"Oregon":{"pvi":-4,"precincts_in":0},"Pennsylvania":{"pvi":-1,"precincts_in":0},"Rhode Island":{"pvi":-6,"precincts_in":0},"South Carolina":{"pvi":7,"precincts_in":0},"South Dakota":{"pvi":16,"precincts_in":0},"Tennessee":{"pvi":14,"precincts_in":0},"Texas":{"pvi":5,"precincts_in":0},"Utah":{"pvi":14,"precincts_in":0},"Vermont":{"pvi":-15,"precincts_in":0},"Virginia":{"pvi":-3,"precincts_in":0},"Washington":{"pvi":-5,"precincts_in":0},"West Virginia":{"pvi":22,"precincts_in":0},"Wisconsin":{"pvi":-1,"precincts_in":0},"Wyoming":{"pvi":25,"precincts_in":0}}
+votes = {"Alabama":{"rVote":0,"dVote":0},"Alaska":{"rVote":0,"dVote":0},"Arizona":{"rVote":0,"dVote":0},"Arkansas":{"rVote":0,"dVote":0},"California":{"rVote":0,"dVote":0},"Colorado":{"rVote":0,"dVote":0},"Connecticut":{"rVote":0,"dVote":0},"Delaware":{"rVote":0,"dVote":0},"Florida":{"rVote":0,"dVote":0},"Georgia":{"rVote":0,"dVote":0},"Hawaii":{"rVote":0,"dVote":0},"Idaho":{"rVote":0,"dVote":0},"Illinois":{"rVote":0,"dVote":0},"Indiana":{"rVote":0,"dVote":0},"Iowa":{"rVote":0,"dVote":0},"Kansas":{"rVote":0,"dVote":0},"Kentucky":{"rVote":0,"dVote":0},"Louisiana":{"rVote":0,"dVote":0},"Maine":{"rVote":0,"dVote":0},"Maryland":{"rVote":0,"dVote":0},"Massachusetts":{"rVote":0,"dVote":0},"Michigan":{"rVote":0,"dVote":0},"Minnesota":{"rVote":0,"dVote":0},"Mississippi":{"rVote":0,"dVote":0},"Missouri":{"rVote":0,"dVote":0},"Montana":{"rVote":0,"dVote":0},"Nebraska":{"rVote":0,"dVote":0},"Nevada":{"rVote":0,"dVote":0},"New Hampshire":{"rVote":0,"dVote":0},"New Jersey":{"rVote":0,"dVote":0},"New Mexico":{"rVote":0,"dVote":0},"New York":{"rVote":0,"dVote":0},"North Carolina":{"rVote":0,"dVote":0},"North Dakota":{"rVote":0,"dVote":0},"Ohio":{"rVote":0,"dVote":0},"Oklahoma":{"rVote":0,"dVote":0},"Oregon":{"rVote":0,"dVote":0},"Pennsylvania":{"rVote":0,"dVote":0},"Rhode Island":{"rVote":0,"dVote":0},"South Carolina":{"rVote":0,"dVote":0},"South Dakota":{"rVote":0,"dVote":0},"Tennessee":{"rVote":0,"dVote":0},"Texas":{"rVote":0,"dVote":0},"Utah":{"rVote":0,"dVote":0},"Vermont":{"rVote":0,"dVote":0},"Virginia":{"rVote":0,"dVote":0},"Washington":{"rVote":0,"dVote":0},"West Virginia":{"rVote":0,"dVote":0},"Wisconsin":{"rVote":0,"dVote":0},"Wyoming":{"rVote":0,"dVote":0}}
+for state in precincts_in:
+    precincts_in[state]["rVote"] = estimated_votes_approval
+    precincts_in[state]["dVote"] = opposite_total
+    precincts_in[state]["rVote"] += precincts_in[state]["pvi"] / 2
+    precincts_in[state]["dVote"] -= precincts_in[state]["pvi"] / 2
+
+opposite_total = 1 - (random.randint(90,300) * 0.0001) - estimated_votes_approval
+print(opposite_total)
+
+while (total_ticks < 10) :
+    total_ticks += 1
+    time.sleep(1 / tick_speed)
+    print(total_ticks)
